6bbee93d80218d486f495b81a6b65f227f81ca0a
Zheng Linxuan <zhenglinxuan@hotmail.com>
2021-04-28 14:41:28 +0800

    Improve EvaluationTable&TableItem model structure. Add method GetEvaluationTable.

--- model/evaluation_table.go
+++ model/evaluation_table.go
@@ -11,15 +11,32 @@ type EvaluationTable struct {
 	gorm.Model
 	HomeworkID int `gorm:"type:int;not null"`
 	TeamID     int `gorm:"type:int;not null"`
+	TableItems []EvaluationTableItem
 }
 
 // EvaluationTableItem 评审表项模型
 type EvaluationTableItem struct {
 	gorm.Model
-	EvaluationTableID int    `gorm:"type:int;not null"`
+	EvaluationTableID uint
 	Content           string `gorm:"type:varchar(255);not null"`
 	Score             int    `gorm:"type:int;not null"`
-	Suggest           string `gorm:"type:text"`
+	Description       string `gorm:"type:text"`
 	ParentItemID      int    `gorm:"type:int;not null"`
 	Sequence          int    `gorm:"type:int;not null"`
 }
+
+// GetEvaluationTable 获取评审表
+func (Repo *Repository) GetEvaluationTable(ID uint) (EvaluationTable, error) {
+	var table EvaluationTable
+	tableResult := Repo.DB.First(&table, ID)
+	if tableResult.Error != nil {
+		return EvaluationTable{}, tableResult.Error
+	}
+	err := Repo.DB.Model(&table).Association("TableItems").Find(&table.TableItems)
+	if err != nil {
+		return EvaluationTable{}, err
+	}
+
+	return table, nil
+}
+
