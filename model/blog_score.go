953efea0295bcb6e621ef3a4863f0ed33571e97d
shican45 <49781724+shican45@users.noreply.github.com>
2021-04-29 22:46:53 +0800

    Add files via upload

--- model/blog_score.go
+++ model/blog_score.go
@@ -21,3 +21,36 @@ type TeamBlogScore struct {
 	ScorekeeperID int `gorm:"type:int;not null"`
 	Grade         int `gorm:"type:int;not null"`
 }
+
+// GetPersonalBlogScoreByID 用ID获取个人博客成绩
+func (Repo *Repository) GetPersonalBlogScoreByID(ID int) (PersonalBlogScore, error) {
+	var personal_blog_score PersonalBlogScore
+	result := Repo.DB.Where("ID = ?", ID).Find(&personal_blog_score)
+	return personal_blog_score, result.Error
+}
+
+// GetPersonalBlogScoreByID 用ID获取团队博客成绩
+func (Repo *Repository) GetTeamBlogScoreByID(ID int) (TeamBlogScore, error) {
+	var team_blog_score TeamBlogScore
+	result := Repo.DB.Where("ID = ?", ID).Find(&team_blog_score)
+	return team_blog_score, result.Error
+}
+
+// GetPersonalBlogScores 获取全部个人博客成绩    仍需修改！
+func (Repo *Repository) GetPersonalBlogScores(ID int) (PersonalBlogScore, error) {
+	var personal_blog_score PersonalBlogScore
+	result := Repo.DB.Where("ID = ?", ID).Find(&personal_blog_score)
+	return personal_blog_score, result.Error
+}
+
+// SetPersonalBlogScoreByID 根据ID设置个人博客成绩
+func (Repo *Repository) SetPersonalBlogScoreByID(ID int, grade int) (int64, error) {
+	result := Repo.DB.Model(&PersonalBlogScore{}).Where("ID = ?", ID).Update("grade", grade)
+	return result.RowsAffected, result.Error
+}
+
+// SetTeamBlogScoreByID 根据ID设置团队博客成绩
+func (Repo *Repository) SetTeamBlogScoreByID(ID int, grade int) (int64, error) {
+	result := Repo.DB.Model(&TeamBlogScore{}).Where("ID = ?", ID).Update("grade", grade)
+	return result.RowsAffected, result.Error
+}
--- model/work_submission.go
+++ model/work_submission.go
@@ -14,3 +14,40 @@ type WorkSubmission struct {
 	SubmitStatus uint8  `gorm:"type:int;not null"`
 	Filepath     string `gorm:"type:varchar(255)"`
 }
+
+// CreateWorkSubmission 创建作业提交表
+func (Repo *Repository) CreateWorkSubmission(SubmitterID int, HomeworkID int,
+	SubmitStatus uint8, Filepath string) (int64, error) {
+	work_submission := WorkSubmission{SubmitterID: SubmitterID, HomeworkID: HomeworkID,
+		SubmitStatus: SubmitStatus, Filepath: Filepath}
+	result := Repo.DB.Create(&work_submission)
+	return result.RowsAffected, result.Error
+}
+
+// GetWorkSubmissionByID 根据ID获取作业提交表
+func (Repo *Repository) GetWorkSubmissionByID(ID int) (WorkSubmission, error) {
+	var work_submission WorkSubmission
+	result := Repo.DB.Where("ID = ?", ID).Find(&work_submission)
+	return work_submission, result.Error
+}
+
+// GetWorkSubmissionBySubmitterIDandHomeworkID 根据SubmitterID和HomeworkID获取作业提交表
+func (Repo *Repository) GetWorkSubmissionBySubmitterIDandHomeworkID(SubmitterID int,
+	HomeworkID int) (WorkSubmission, error) {
+	var work_submission WorkSubmission
+	result := Repo.DB.Where(map[string]interface{}{"SubmitterID": SubmitterID,
+		"HomeworkID": HomeworkID}).Find(&work_submission)
+	return work_submission, result.Error
+}
+
+// SetSubmitStatusByID 根据ID设置作业提交状态
+func (Repo *Repository) SetSubmitStatusByID(ID, submit_status int) (int64, error) {
+	result := Repo.DB.Model(&WorkSubmission{}).Where("ID = ?", ID).Update("submit_status", submit_status)
+	return result.RowsAffected, result.Error
+}
+
+// DeleteWorkSubmissionByID 根据ID删除作业提交表
+func (Repo *Repository) DeleteWorkSubmissionByID(ID int) error {
+	result := Repo.DB.Delete(&WorkSubmission{}, ID)
+	return result.Error
+}
